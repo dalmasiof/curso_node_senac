2f2131a02cd0d288ad917e0cbd283ce6e6eee022
DALMASIO FERNANDES DE OLIVEIRA FILHO <dalmasio.fofilho@senacsp.edu.br>
2025-11-01 11:04:08 -0300

    add POO

--- POO/menu.js
+++ POO/menu.js
@@ -11,50 +11,93 @@ class Pessoa {
     #senha;
     #respostaChavePessoa;
 
-    constructor(nome, idade, dataNasc, senha, respostaChavePessoa) {
+    constructor(nome, idade, senha, respostaChavePessoa) {
         this.nome = nome;
         this.idade = idade;
-        this.dataNasc = new Date(dataNasc).toLocaleDateString("pt-br");
         this.#senha = senha;
-        this.#respostaChavePessoa = respostaChavePessoa
+        this.#respostaChavePessoa = respostaChavePessoa;
     }
 
-    apresentar(){
+    apresentar() {
         console.log(`\nOlá, meu nome é ${this.nome}, sou nascido em ${this.dataNasc} e tenho ${this.idade} de idade.\n`);
     }
 
-    get senha(){
+    get senha() {
         let respostaChavePessoa = promptSync("Qual o nome do seu PET? ");
-        
-        if(respostaChavePessoa == this.#respostaChavePessoa){
+
+        if (respostaChavePessoa == this.#respostaChavePessoa) {
             console.log(`Senha privada: ${this.#senha}`);
         }
-        else{
+        else {
             console.log(`Resposta incorreta!`);
         }
     }
 }
 
-let nomePessoa = promptSync("Digite seu nome: ");
-let idadePessoa = promptSync("Digite sua idade: ");
-let dataNascimentoPessoa = promptSync("Digite sua data de nascimento no formato ano-mês-dia: ");
-let senhaPessoa = promptSync("Digite sua senha: ");
-let respostaChavePessoa = promptSync("Qual o nome do seu PET? ");
+class Aluno extends Pessoa {
 
-const novaPessoa = new Pessoa(
-    nomePessoa,
-    idadePessoa,
-    //new Date(1997,11,21),
-    dataNascimentoPessoa,
-    senhaPessoa,
-    respostaChavePessoa
-);
+    constructor(nome, idade, ra) {
+        super(nome, idade);
+        this.ra = ra;
+    }
 
+    apresentar() {
+        console.log(`\nOlá, meu nome é ${this.nome}, tenho ${this.idade} anos, sou aluno e meu RA é ${this.ra}.`);
+    }
 
-//ver senha
-let verSenha = promptSync("Deseja ver sua senha? S/N ");
+}
 
-if(verSenha == 'S')
-    novaPessoa.senha;
+class Professor extends Pessoa {
 
-console.log(novaPessoa);
+    constructor(nome, idade, salario, registro) {
+        super(nome, idade)
+        this.salario = salario;
+        this.registro = registro;
+    }
+
+    apresentar() {
+        console.log(`\nOlá, meu nome é ${this.nome}, tenho ${this.idade} anos, sou professor, meu salario é ${this.salario} e meu registro é ${this.registro}.`);
+    }
+}
+
+let tipoPessoa = parseInt(promptSync("Digite 1 para professor ou digite 2 para aluno: "));
+
+let pessoa;
+
+if (tipoPessoa === 1) {
+    let nomePessoa = promptSync("Digite seu nome: ");
+    let idadePessoa = promptSync("Digite sua idade: ");
+
+    let salario = promptSync("Qual o seu salario? ");
+    let registro = promptSync("QUal o seu registro? ");
+
+    pessoa = new Professor(
+        nomePessoa,
+        idadePessoa,
+        salario,
+        registro
+    );
+
+    console.log("\nBem vindo novo professor:");
+}
+
+else if (tipoPessoa === 2) {
+    let nomePessoa = promptSync("Digite seu nome: ");
+    let idadePessoa = promptSync("Digite sua idade: ");
+
+    let ra = promptSync("Qual seu RA? ");
+
+    pessoa = new Aluno(
+        nomePessoa,
+        idadePessoa,
+        ra
+    );
+}
+
+else {
+    console.log("\nOpção inválida, digite 1 ou 2, tente novamente.\n");
+    process.exit(0);
+}
+
+pessoa.apresentar()
+console.log("\n");
